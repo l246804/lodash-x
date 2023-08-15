f8d2baa9de3d3fd37815ebe429f22f1042c5745e
Ray Hao <2468048176@qq.com>
2023-08-15 17:15:16 +0800

    feat: 🎸 add compactObject

---
+++ src/compactObject/index.ts
@@ -0,0 +1,17 @@
+import type { Falsey } from 'lodash'
+import { fromPairs, toPairs } from 'lodash-unified'
+
+/**
+ * 对象版本 `compact`
+ *
+ * @example
+ * ```ts
+ * compactObject({ a: 1, b: 0, c: false, d: '' })
+ * // => { a: 1 }
+ * ```
+ */
+export default function compactObject<const T extends object>(object: T) {
+  return fromPairs(toPairs(object).filter(([_, v]) => !!v)) as {
+    -readonly [K in keyof T as T[K] extends Falsey ? never : K]: T[K]
+  }
+}
